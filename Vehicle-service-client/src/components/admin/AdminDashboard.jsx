2ad36e807bc46a4332038a2487edbe0615a8c985
Nithinlinga <nithinlinga2@gmail.com>
2025-09-08 09:10:59 +0530

    testing config

--- Vehicle-service-client/src/components/admin/AdminDashboard.jsx
+++ Vehicle-service-client/src/components/admin/AdminDashboard.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <>
+      <div className='pt-5 pl-20  w-screen flex space-x-4'>
+        <button className=' rounded-sm dark:bg-cyan-500 p-2'>Manage Service Centres</button>
+        <button className=' rounded-sm dark:bg-cyan-500 p-2'>Manage Appointments</button>
+      </div>
+    </>
   )
 }
 
